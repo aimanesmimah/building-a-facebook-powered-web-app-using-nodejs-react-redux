e185529cd110d636210319c6e8ecbeab5542fc48
cloudstrife9494 <aymansmimah@yahoo.fr>
2017-11-18 11:30:09 +0100

    frontend: adding global style

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import storeFactory from './store/index';
+import './theme/globalStyle';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
---
+++ frontend/src/theme/globalStyle.jsx
@@ -0,0 +1,11 @@
+import {injectGlobal} from 'styled-components';
+
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Patua+One|Raleway');
+
+  body{
+    font-family: 'Raleway', sans-serif;
+    font-weight: 600;
+  }
+
+`
